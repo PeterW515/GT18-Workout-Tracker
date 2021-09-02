8f1a3bccf0882617dad3286f91d15b6f8015cebf
Peter Weigman <phweigman@gmail.com>
2021-09-01 22:06:02 -0400

    fixed get route summing total duration

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 const db = require('../../models');
 
-router.get('/', async (req,res) => {
-    try{
-        const data = await db.Workout.find({});
+router.get('/', async (req, res) => {
+    try {
+        //const data = await db.Workout.find({});
+        //use aggregate to sum fields for total duration
+        const data = await db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" }
+                }
+            }
+        ]);
         res.json(data);
     } catch (err) {
         res.status(500).json(err);
